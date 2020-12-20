f270df6dcb50ccdd44b0f4671d6161753319055f
Jaakko Pallari <jkpl@lepovirta.org>
2020-12-20 20:02:00 +0200

    part 3: mongodb integration in backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,84 +1,95 @@
-const generateId = require('./genid');
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const Person = require('./models/person');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-];
-
-app.use(express.json());
 app.use(express.static('build'))
+app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   const timestamp = new Date();
-  const content = `Phonebook has info for ${persons.length} people
+  Person.find({}).then((persons) => {
+    const content = `Phonebook has info for ${persons.length} people
 
 ${timestamp.toDateString()} ${timestamp.toTimeString()}`;
-  response.send(content);
+    response.send(content);
+  }).catch((err) => next(err));
 });
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+  Person
+    .find({})
+    .then((persons) => response.json(persons))
+    .catch((err) => next(err));
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id.toString() === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
+app.get('/api/persons/:id', (request, response, next) => {
+  Person
+    .findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)        
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
+});
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then((result) => response.status(204).end())
+    .catch((err) => next(err));
+});
+
+app.post('/api/persons', (request, response, next) => {
+  const person = new Person({
+    name: request.body.name,
+    number: request.body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => response.json(savedPerson))
+    .catch((err) => next(err))
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const person = {
+    name: request.body.name,
+    number: request.body.number,
+  };
+  
+  Person
+    .findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => response.json(updatedPerson))
+    .catch(err => next(err));
+});
+
+function unknownEndpoint(request, response) {
+  response.status(404).send({ error: 'unknown endpoint' });
+}
+app.use(unknownEndpoint);
+
+function errorHandler(err, request, response, next) {
+  console.error(`${err.name}: ${err.message}`);
+
+  if (err.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
   }
-});
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id.toString() !== id);
-  response.status(204).end();
-});
-
-app.post('/api/persons', (request, response) => {
-  const person = {...request.body, id: generateId()};
-  if (!person.name) {
-    response.status(400).json({error: 'missing name field'});
-    return;
-  }
-  if (!person.number) {
-    response.status(400).json({error: 'missing number field'});
-    return;
-  }
-  if (persons.find((p) => p.name === person.name)) {
-    response.status(400).json({error: 'name must be unique'});
-    return;
+  if (err.name === 'ValidationError') {
+    return response.status(400).json({ error: err.message });
   }
 
-  persons.push(person);
-  response.json(person);
-});
+  next(err);
+}
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
