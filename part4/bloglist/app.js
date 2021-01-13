9f1620d602306e5b26788cea818b815228e7829a
Jaakko Pallari <jkpl@lepovirta.org>
2021-01-13 19:19:42 +0200

    part4: user creation doesn't require auth

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -27,9 +27,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/login', auth.login);
+app.use('/api/users', require('./users'));
 app.use(auth.authorize);
 app.use('/api/blogs', require('./blogs'));
-app.use('/api/users', require('./users'));
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
