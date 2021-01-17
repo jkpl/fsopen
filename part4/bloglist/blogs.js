75bd75dbb32ed918c49f03b84720ca9ce7492b05
Jaakko Pallari <jkpl@lepovirta.org>
2021-01-17 13:30:48 +0200

    Populate returned blogs with the user details

--- part4/bloglist/blogs.js
+++ part4/bloglist/blogs.js
@@ -1,38 +1,43 @@
 const router = require('express').Router();
 const Blog = require('./models/blog');
-const User = require('./models/user');
+
+function blogFromBody(body) {
+  return {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  };
+}
 
 router.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user');
-  response.json(blogs);
+  response.json(blogs.map((b) => b.toJSON()));
 });
 
 router.post('/', async (request, response) => {
-  const blog = new Blog({...request.body, user: request.user.id});
+  const blog = new Blog({...blogFromBody(request.body), user: request.user.id});
   const result = await blog.save();
-  response.status(201).json(result);
+  const resultWithUser = await result.populate('user').execPopulate()
+  response.status(201).json(resultWithUser.toJSON());
 });
 
 router.delete('/:id', async (request, response) => {
   const blogToDelete = await Blog.findById(request.params.id);
   if (blogToDelete.user.toString() === request.user.id) {
-    const result = await Blog.findByIdAndRemove(request.params.id);
-    response.status(204).end();  
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
   } else {
     response.status(401).json({error: 'Deletion of other\'s blogs is not allowed.'})
   }
 });
 
 router.put('/:id', async (request, response) => {
-  const blog = {
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes,
-  };
-
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  response.json(updatedBlog);
+  const blog = blogFromBody(request.body);
+  const updatedBlog = await Blog
+    .findByIdAndUpdate(request.params.id, blog, { new: true })
+    .populate('user')
+  response.json(updatedBlog.toJSON());
 });
 
 module.exports = router;
